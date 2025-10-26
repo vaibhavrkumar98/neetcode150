01764d14d5c8512bb1d327e680afb944cc664765
Vaibhav R. Kumar <203496523+vaibhavrkumar98@users.noreply.github.com>
2025-10-25 23:23:36 -0700

    using cli

---
+++ twoSum.py
@@ -0,0 +1,10 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # create hashmap
+        hashMap = {}
+        # itr ovr nums
+        for i,n in enumerate(nums):
+            diff  = target - n
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[n] = i #if no match, make an entry in hashMap
+        return
