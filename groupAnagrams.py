fcc19fae81664e9e76bdb1c20640ac4d7ffd8789
Vaibhav R. Kumar <203496523+vaibhavrkumar98@users.noreply.github.com>
2025-10-31 19:19:41 +0530

    group anagrams

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,1 +1,15 @@
 # groupAnagrams - medium
+from collections import defaultdict
+class Solution:
+res = defaultdict(list)
+        # res = []
+
+        for s in strs:
+            count = [0] * 26
+
+            for c in s:
+                count[ord(c) - ord("a")] +=1
+
+            res[tuple (count)].append(s)
+    
+        return list (res.values())
